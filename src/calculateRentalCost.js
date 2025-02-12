9eb28044875b63ef122ee3e8defa218ef5f432ba
Lev <levsylik@gmail.com>
2025-02-12 22:15:21 +0300

    Update

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,15 +4,14 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  const dailyRate = 40;
+  const DAILY_RATE = 40;
+  const DISCOUNT_7_DAYS = 50;
+  const DISCOUNT_3_DAYS = 20;
 
-  let totalCost = days * dailyRate;
+  const totalCost = days * DAILY_RATE;
 
-  if (days >= 7) {
-    totalCost -= 50;
-  } else if (days >= 3) {
-    totalCost -= 20;
-  }
+  if (days >= 7) return totalCost - DISCOUNT_7_DAYS;
+  if (days >= 3) return totalCost - DISCOUNT_3_DAYS;
 
   return totalCost;
 }
